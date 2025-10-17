93fad9d89dc5c4ae807e85162177bca29f2fe936
Sin-Yejun <sinyejun2@gmail.com>
2025-10-17 09:58:05 +0900

    feat: 파일 정리 및 중량비율 추가

--- src/format_script/csv_to_parquet.py
+++
@@ -1,8 +0,0 @@
-# pip install pandas pyarrow
-import pandas as pd
-
-csv_path = "data/02_processed/weekly_streak_dataset.csv"
-parquet_path = "data/02_processed/parquet/weekly_streak_dataset.parquet"
-
-df = pd.read_csv(csv_path)  # 필요시 dtype={"colA": "int64"} 등 지정
-df.to_parquet(parquet_path, index=False, compression="snappy")  # snappy/zstd/gzip
--- src/format_script/ndjson_to_parquet.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-file_name = "user"
-duckdb.sql(f"""
-COPY (
-  SELECT * FROM read_ndjson_auto('data/json/{file_name}.ndjson')
-) TO 'data/parquet/{file_name}.parquet' (FORMAT PARQUET, COMPRESSION ZSTD, OVERWRITE_OR_IGNORE 1);
-""")
-
-print(f"✅ Parquet 생성 완료: data/parquet/{file_name}.parquet")
